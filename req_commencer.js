805d77317f87a6515ef726ea87725a6453bbb28d
KUNKELA <KUNKELA@intech-s1>
2020-11-18 11:59:54 +0100

    correction de la req_commencer

--- req_commencer.js
+++ req_commencer.js
@@ -1,19 +1,16 @@
 "use strict"
 
 const fs = require("fs");
-require("remedial");
 
 const traits = function(req, res, query) {
 let page;
 
 
-	if(pathname === "/req_commencer") {
 	
 		page = fs.readFileSync("modele_salle_attente.html","utf-8");
 		res.writeHead(200, {"Content-Type": "text/html"});
 		res.write(page);
 		res.end();
-	}
 
 };
 

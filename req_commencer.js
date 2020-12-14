184e12aaf26bf7c26504855b63f55ada5d02c6f5
KUNKELA <KUNKELA@intech-s1>
2020-12-14 11:01:19 +0100

    restauration des req_commencer et req_attendre

--- req_commencer.js
+++ req_commencer.js
@@ -1,33 +1,52 @@
-"use strict"
-
-const fs = require("fs");
-require("remedial");
+"use strict";
 
 const afficher_liste_joueurs = require("./fct_afficher_liste_lobby.js");
 
 const traits = function(req, res, query) {
+const fs = require("fs");
+require("remedial");
 	let page;
-    let joueurs;
-    let list_joueurs;
-    let page_jeu_actif;
-    let page_jeu_passif;
-    let page_attente;
-    let listeMembres;
-    let contenu_fichier;
+	let contenu;
+	let _joueurs; 
+    let i;
+	let chaine;
+	let list_joueurs;
 	let marqueurs;
 
+contenu = fs.readFileSync("joueurs.json","utf-8");
 
-	list_joueurs = [];
+// supprime le contenu du joueurs.json (utilisé pour une nouvelle partie).
 
-	joueurs = fs.readFileSync("joueurs.json", "UTF-8");
-	if (typeof joueurs === 'string' && joueurs !== "") {
-	list_joueurs = JSON.parse(joueurs);
+if(JSON.parse(contenu).role.length === 0 ) {
+	fs.writeFileSync("joueurs.json", "", "utf-8");
+	contenu = fs.readFileSync("joueurs.json","utf-8");
+	}
+
+//utilisation du fichier tamplate .
+
+if(contenu !== "") {  
+	list_joueurs =  JSON.parse(contenu); //utilisation de joueur.json pour le 2e joueurs
+	}else 	{
+	list_joueurs = JSON.parse(fs.readFileSync("_joueurs.json","utf-8")); //utilisation de _joueurs.json pour 1e 
 	}
 	
-	list_joueurs.push(query.pseudo);
+	// on ajoute le pseudo, le role et l'etat dans la liste.
 	
-	joueurs = JSON.stringify(list_joueurs);
-	fs.writeFileSync("joueurs.json", joueurs, "utf-8");
+	i = Math.floor(Math.random() * list_joueurs.role.length);
+
+	_joueurs = {};	
+	_joueurs.pseudo = query.pseudo;
+	_joueurs.etat = "Attente";
+	_joueurs.role = list_joueurs.role[i];
+	
+	list_joueurs.role.splice(i, 1);
+	list_joueurs.participant.push(_joueurs);	
+	
+	// Enregistrement des information dans le fichier joueurs.json.
+
+    chaine = JSON.stringify(list_joueurs);
+    fs.writeFileSync("joueurs.json",chaine,"utf-8");
+
 
 
 	marqueurs = {};

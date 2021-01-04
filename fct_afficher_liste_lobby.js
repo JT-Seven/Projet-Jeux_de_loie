2ab04f01e2f5c300e7bb8893ca432565bdde76e0
KUNKELA <KUNKELA@intech-s1>
2021-01-04 11:48:29 +0100

    .

--- fct_afficher_liste_lobby.js
+++ fct_afficher_liste_lobby.js
@@ -1,16 +1,16 @@
 "use strict"
 
-const afficher_liste_joueurs = function (list_joueurs) {
+const afficher_liste_lobby = function (list_joueurs) {
 	let html;
 	let i;
 
 	html = "<ul>";
-	for (i = 0; i < list_joueurs.participant.length; i++) {
-		html += "<li>" + list_joueurs.participant[i].pseudo + "</li>";
+	for (i = 0; i < lobby.joueurs.length; i++) {
+		html += "<li>" + lobby.joueurs[i].pseudo + "</li>";
 	}
 	
 	html += "<ul>";
 	return html;
 };
 
-module.exports = afficher_liste_joueurs;
+module.exports = afficher_liste_lobby;
--- req_commencer.js
+++ req_commencer.js
@@ -1,58 +1,26 @@
 "use strict";
 
-const afficher_liste_joueurs = require("./fct_afficher_liste_lobby.js");
+//const afficher_liste_lobby = require("./fct_afficher_liste_lobby.js");
 
 const traits = function(req, res, query) {
 const fs = require("fs");
 require("remedial");
 	let page;
-	let contenu;
-	let _joueurs; 
-    let i;
-	let chaine;
-	let list_joueurs;
+	let lobby = [];
+	let listlobby;
 	let marqueurs;
+	let joueurs;
+	let list_joueurs;
 
-contenu = fs.readFileSync("joueurs.json","utf-8");
-
-// supprime le contenu du joueurs.json (utilisé pour une nouvelle partie).
-
-if(JSON.parse(contenu).role.length === 0 ) {
-	fs.writeFileSync("joueurs.json", "", "utf-8");
-	contenu = fs.readFileSync("joueurs.json","utf-8");
-	}
-
-//utilisation du fichier tamplate.
-
-if(contenu !== "") {  
-	list_joueurs =  JSON.parse(contenu); 
-
-//utilisation de joueur.json pour le 2e joueurs.
-
-	} else {
-	list_joueurs = JSON.parse(fs.readFileSync("_joueurs.json","utf-8"));//utilisation de _joueurs.json pour 1e 
-	}
+joueurs = {};
+joueurs.pseudo = query.pseudo;
+lobby.push(joueurs);
+lobby = JSON.stringify(fs.writeFileSync("lobby.json",lobby,"utf-8"));
 	
-	// on ajoute le pseudo, le role et l'etat dans la liste.
 	
-	i = Math.floor(Math.random() * list_joueurs.role.length);
-
-	_joueurs = {};	
-	_joueurs.pseudo = query.pseudo;
-	_joueurs.etat = "Attente";
-	_joueurs.role = list_joueurs.role[i];
-	
-	list_joueurs.role.splice(i, 1);
-	list_joueurs.participant.push(_joueurs);	
-	
-	// Enregistrement des information dans le fichier joueurs.json.
-
-    chaine = JSON.stringify(list_joueurs);
-    fs.writeFileSync("joueurs.json",chaine,"utf-8");
-
 	marqueurs = {};
 	marqueurs.pseudo = query.pseudo;
-	marqueurs.liste = afficher_liste_joueurs(list_joueurs);
+//	marqueurs.liste = afficher_liste_lobby(list_joueurs);
 
 	page = fs.readFileSync("modele_salle_attente.html","utf-8");
 	page = page.supplant(marqueurs);

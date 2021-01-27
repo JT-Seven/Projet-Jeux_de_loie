275aa6e9f4e1198980471aabacb309cd662b400d
TERNEUS <TERNEUS@intech-s1>
2021-01-27 03:45:19 +0100

    FIN des modifications pour les pages HTML/CSS

--- req_mdp_oublier.js
+++ req_mdp_oublier.js
@@ -10,68 +10,137 @@ const trait = function (req, res, query) {
     let listeMembres;
     let i;
     let trouve;
+	let ajout;
+	let question;
+	let reponse;
 
     contenu_fichier = fs.readFileSync("membres.json", 'utf-8');
     listeMembres = JSON.parse(contenu_fichier);
 
     trouve = false;
+	question = false;
+	reponse = false;
     i = 0;
-    while (i < listeMembres.length && trouve === false) {
+    while (i < listeMembres.length && trouve === false && question === false && reponse === false) {
         if (listeMembres[i].pseudo === query.pseudo) {
-            if (listeMembres[i].password === query.password) {
-				if (listeMembres[i].password2 === query.password2) {
-                trouve = true;
-            }
-            } 
+        	trouve = true;
+        } else {
+			trouve = false;
+		} 
+		if (listeMembres[i].reponse === query.reponse) {
+            trouve = true;
+			reponse = true;
+        } else {
+			reponse = false;
+		}
+		 if (listeMembres[i].question === query.selection_question) {
+            trouve = true;
+			question = true;
+        } else {
+			question = false;
         }
+
         i++;
     }
 
-	if	(query.nouveau_mdp === query.confirmation_mdp && trouve === true) {
-		
+	if	(trouve === true && question === true && reponse === true) {
+
 		page = fs.readFileSync('modele_mdp_oublier.html', 'utf-8');
 
 		marqueurs = {};
 		marqueurs.confirmation = "Votre mot de passe a bien été modifié !";
 		marqueurs.erreur = "";
-		marqueurs.pseudo = query.pseudo;
-		marqueurs.password = query.password;
-		marqueurs.password2 = query.password2;
+		marqueurs.pseudo = "";
+		marqueurs.password = "";
+		marqueurs.password2 = "";
+		marqueurs.reponse = "";
 		page = page.supplant(marqueurs);
-		listeMembres[listeMembres.length] = marqueurs;
+
+
+		ajout = {};
+		ajout.confirmation = "Mot de Passe modifié !";
+		ajout.pseudo = query.pseudo;
+		ajout.password = query.confirmation_mdp;
+		ajout.question = query.selection_question;
+		ajout.reponse = query.reponse;
+		listeMembres[listeMembres.length] = ajout;
 		listeMembres[i-1].password = query.confirmation_mdp;
 		
 		contenu_fichier = JSON.stringify(listeMembres);
 
 		fs.writeFileSync("membres.json", contenu_fichier, 'utf-8');
-
+	
 	} else {
 		
-		if (trouve === false) {
+		page = fs.readFileSync('modele_mdp_oublier.html', 'utf-8');
 
-			page = fs.readFileSync('modele_mdp_oublier.html', 'utf-8');
+		marqueurs = {};
+		marqueurs.confirmation = "";
+		marqueurs.erreur = "ERREUR : Dans vos changement !";
+		marqueurs.pseudo = "";
+		marqueurs.password = "";
+		marqueurs.password2 = "";
+		marqueurs.reponse = "";
 
-			marqueurs = {};
-			marqueurs.erreur = "ERREUR : ce compte n'existe pas";
-			marqueurs.password = "";
-			marqueurs.password2 = "";
-			marqueurs.pseudo = "";
-			marqueurs.confirmation = "";
-			page = page.supplant(marqueurs);
-
-		} else {
-
-			page = fs.readFileSync('modele_mdp_oublier.html', 'utf-8');
-
-			marqueurs = {};
-			marqueurs.erreur = "ERREUR : Votre nouveau mot de passe et la confirmation du nouveau mot de passe ne sont pas cohérent !";
-			marqueurs.confirmation = "";
-			marqueurs.pseudo = "";
-			marqueurs.password = "";
-			marqueurs.password2 = "";
-			page = page.supplant(marqueurs);
-		}
 	}
+
+	if (trouve === true && query.nouveau_mdp !== query.confirmation_mdp) {
+		
+		page = fs.readFileSync('modele_mdp_oublier.html', 'utf-8');
+
+        marqueurs = {};
+        marqueurs.erreur = "ERREUR : Votre nouveau mot de passe et la confirmation du nouveau mot de passe ne sont pas cohérent !";
+        marqueurs.confirmation = "";
+        marqueurs.pseudo = "";
+        marqueurs.password = "";
+        marqueurs.password2 = "";
+        page = page.supplant(marqueurs);
+	
+	} 
+
+	if (question === false) {
+
+        page = fs.readFileSync('modele_mdp_oublier.html', 'utf-8');
+
+        marqueurs = {};
+        marqueurs.erreur = "ERREUR : choix de question est incorrecte";
+		marqueurs.password = "";
+        marqueurs.password2 = "";
+        marqueurs.pseudo = "";
+        marqueurs.confirmation = "";
+        page = page.supplant(marqueurs);
+
+    } 
+
+
+	if (reponse === false) {
+
+        page = fs.readFileSync('modele_mdp_oublier.html', 'utf-8');
+
+        marqueurs = {};
+        marqueurs.erreur = "ERREUR : la reponse est incorecte";
+        marqueurs.password = "";
+        marqueurs.password2 = "";
+        marqueurs.pseudo = "";
+        marqueurs.confirmation = "";
+        page = page.supplant(marqueurs);
+
+    }
+
+
+	if (trouve === false) {
+
+        page = fs.readFileSync('modele_mdp_oublier.html', 'utf-8');
+
+        marqueurs = {};
+        marqueurs.erreur = "ERREUR : ce compte n'existe pas";
+        marqueurs.password = "";
+        marqueurs.password2 = "";
+        marqueurs.pseudo = "";
+        marqueurs.confirmation = "";
+        page = page.supplant(marqueurs);
+
+    }
 
 		res.writeHead(200, { 'Content-Type': 'text/html' });
 		res.write(page);
